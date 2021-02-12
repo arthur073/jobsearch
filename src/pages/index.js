8db9d79749685d99d286acda79277476adaf9d8c
averger <arthur.verger@murex.com>
2021-02-12 18:46:47 +0100

    ajax call

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import { FaSearch } from "@react-icons/all-files/fa/FaSearch";
 import Layout from '../components/layout/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <main role="main" class="container">
-        <h1 class="text-center mt-5"><FaSearch /></h1>
-        <div class="form-outline mt-4">
-            <input type="search" id="form1" class="form-control form-control-lg" placeholder="Search for any job keyword (i.e. Product Manager)" aria-label="Search" />
-        </div>
-    </main>
+const IndexPage = () => {
+    const Data = "arthur"
 
-  </Layout>
-)
+    const [starsCount, setStarsCount] = useState(0)
+        useEffect(() => {
+            // get data from GitHub api
+            fetch(`https://api.github.com/repos/gatsbyjs/gatsby`)
+            .then(response => response.json()) // parse JSON from request
+            .then(resultData => {
+                setStarsCount(resultData.stargazers_count)
+            }) // set data for the number of stars
+        }, [])
+
+    return (   
+    <Layout>
+        <main role="main" class="container">
+            <h1 class="text-center mt-5"><FaSearch /></h1>
+            <div class="form-outline mt-4">
+                <input type="search" id="form1" class="form-control form-control-lg" placeholder="Search for any job keyword (i.e. Product Manager)" aria-label="Search" />
+            </div>
+            <p>test {Data} {starsCount} </p>
+        </main>
+
+    </Layout>
+    )
+}
 
 export default IndexPage
