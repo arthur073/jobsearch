3137d45182a55f347c98b812dd82554913cc8848
averger <arthur.verger@murex.com>
2021-02-12 18:27:04 +0100

    initial version

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ import { FaHeart } from "@react-icons/all-files/fa/FaHeart";
 
 const Footer = () => (
     <footer class="footer bg-light">
-        <div class="container text-center mt-1 mb-1">
+        <div class="container text-center mt-2 mb-2">
             <span class="text-muted ">Made with <FaHeart /> by Arthur Verger.</span>
         </div>
     </footer>
